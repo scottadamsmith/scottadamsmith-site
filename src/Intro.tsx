19232f073badf53926a8d7d4fd1f76306b899b51
Scott Smith <1824850+scottadamsmith@users.noreply.github.com>
2021-08-29 14:31:42 -0500

    Updated some intro content

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -2,21 +2,21 @@
 import './Intro.scss';
 import IntroImage from './Intro.jpeg';
 
-export default function Into() {
+export default function Intro() {
   return (
     <section className="intro">
       <div className="intro-accent-bg" />
-      <h1 className="heading">
-        <div className="name">Scott Smith</div>
+      <div className="heading">
+        <h1 className="name">Scott Smith</h1>
         <div className="image-container">
           <img src={IntroImage} alt="Me At Red Rocks Amphitheatre" title="Me At Red Rocks Amphitheatre" />
         </div>
-        <div className="description">
-          <h2>I am a Software Engineer focusing on frontend, but comfortable working throughout the stack.
-          My professional experience includes backend, data, and devops, but my passion
-          and focus in recent years has been in the frontend.</h2>
-        </div>
-      </h1>
+        <h2 className="description">
+          Software Engineer that loves problem solving and seeing others benefit from the 
+          things I build or fix. My professional experience includes backend, data, and devops, 
+          but my passion and focus in recent years has been in the frontend.
+        </h2>
+      </div>
     </section>
   );
 }
